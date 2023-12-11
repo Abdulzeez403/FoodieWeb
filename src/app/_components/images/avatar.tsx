e7d1a7bee0a65f609dad5b31c8857b8e757974fc
sodiq <abdulazeezsodiq403@gmail.com>
2023-12-11 12:13:05 +0100

    [update] worked on cart

--- src/app/_components/images/avatar.tsx
+++ src/app/_components/images/avatar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 interface IProps {
-    src: string
+    src: any
     size?: "small" | "default" | "large" | number
     shape?: "square" | "circle"
     active?: boolean
@@ -11,14 +11,12 @@ interface IProps {
 
 const AvatarComponents = ({ size = "default", shape = "circle", src, active = false }: IProps) => {
     return (
-        <div>
-            <Avatar
-                src={src}
-                shape={shape}
-                size={size}
-                alt="image"
-            />
-        </div>
+        <Avatar
+            src={src}
+            shape={shape}
+            size={size}
+            alt="image"
+        />
     )
 }
 
--- src/lib/features/menu/cartSlice.ts
+++ src/lib/features/menu/cartSlice.ts
@@ -5,12 +5,14 @@ interface CartItem {
   items: ICart[];
   loading: boolean;
   error: string | null;
+  counter: number;
 }
 
 const initialState: CartItem = {
   items: [],
   loading: false,
   error: null,
+  counter: 0,
 };
 
 export const CartSlice = createSlice({
@@ -26,7 +28,14 @@ export const CartSlice = createSlice({
     clearCart: (state) => {
       state.items = [];
     },
+    increment: (state) => {
+      state.counter += 1;
+    },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
   },
 });
-export const { addToCart, removeItem, clearCart } = CartSlice.actions;
+export const { addToCart, removeItem, clearCart, increment, decrement } =
+  CartSlice.actions;
 export default CartSlice.reducer;
